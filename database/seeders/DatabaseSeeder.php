8b9dfb54b2b066a0a53bf548dbc77d24ed6e825f
Ahmad Sabili <ahmadsabili58@gmail.com>
2022-12-07 19:46:36 +0700

    create camp benefit and admin role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CampTableSeeder::class,
+            CampBenefitTableSeeder::class,
+            UserTableSeeder::class,
         ]);
     }
 }
